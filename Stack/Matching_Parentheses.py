21daff13d8f976fa8623a45e1393f369575e8b80
Nitesh kumar <30889903+smartnitesh@users.noreply.github.com>
2019-08-20 23:47:45 +0530

    Create Matching_Parentheses.py

---
+++ Stack/Matching_Parentheses.py
@@ -0,0 +1,22 @@
+#creating opening list 
+openList = ["[","{","("]
+#creating closong list
+closeList = ["]","}",")"]
+def balance(myStr):
+#create a Empty Stack 
+    stack= []
+    for i in myStr:
+        if i in openList:     
+            stack.append(i)
+        elif i in closeList:
+            pos = closeList.index(i)   
+            if ((len(stack) > 0) and (openList[pos] == stack[len(stack)-1])):
+            #if Both Parentheses are matched than POP element for List
+                stack.pop()
+            else:
+                return "Unbalanced"
+    if len(stack) == 0:
+        return "Balanced"
+ #driver code
+print (balance("{[()](){}}")) 
+print (balance("]{[()](){}}")) 
