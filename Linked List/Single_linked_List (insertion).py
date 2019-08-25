13ed982ae330468f358a6533626b5cbfb7e4ad75
Nitesh kumar <30889903+smartnitesh@users.noreply.github.com>
2019-08-26 00:01:45 +0530

    Update Single_linked_List (insertion).py

--- Linked List/Single_linked_List (insertion).py
+++ Linked List/Single_linked_List (insertion).py
@@ -1,56 +1,48 @@
+#create a claas for Node
 class Node:
-    def __init__(self, data):
+    def __init__(self,data):
         self.data = data
         self.next = None
-
-class LinkedList:
+#create a class for linklist its self
+class linklist:
     def __init__(self):
         self.head = None
+#for return value
+    def peek(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+#for inserting value in end of the list
 
-    def print_list(self):
-        cur_node = self.head
-        while cur_node:
-            print(cur_node.data)
-            cur_node = cur_node.next
-
-    def append(self, data):
+    def append_end(self,data):
         new_node = Node(data)
-
         if self.head is None:
-            self.head = new_node
+            self.head= new_node
             return
-
         last_node = self.head
         while last_node.next:
-            last_node = last_node.next
+            last_node= last_node.next
         last_node.next = new_node
-
-    def prepend(self, data):
+#inserting element in start of list
+    def append_start(self,data):
         new_node = Node(data)
-
-        new_node.next = self.head
+        new_node.next=self.head
         self.head = new_node
-
-    def insert_after_node(self, prev_node, data):
-
+#inserting element in the given element 
+    def insert_in_btw(self,prev_node,data):
         if not prev_node:
-            print("Previous node is not in the list")
-            return 
-
+            print('previous node is not in list')
+            return
         new_node = Node(data)
-
         new_node.next = prev_node.next
-        prev_node.next = new_node
-
-
-llist = LinkedList()
-llist.append("A")
-llist.append("B")
-llist.append("C")
-llist.append("D")
-
-# llist.prepend("E")
-llist.insert_after_node(llist.head.next, "E")
-llist.print_list()
-
- 
+        prev_node.next=new_node     
+     
+linklst = linklist()
+linklst.append_end(10)
+linklst.append_end(20)
+linklst.append_end(40)
+linklst.append_start(100)
+linklst.insert_in_btw(linklst.head.next.next, 10000)
+linklst.peek()
+             
